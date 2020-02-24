2705812ac7ffae8a297f84f2c78d1c329e784f59
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-02-24 10:45:44 -0300

    Criação dos components para separação de responsábilidades

---
+++ expenses/lib/components/transaction_form.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class TransactionForm extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                labelText: 'Título',
+              ),
+            ),
+            TextField(
+              controller: valueController,
+              decoration: InputDecoration(
+                labelText: 'Valoe (R\$)',
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                FlatButton(
+                  child: Text(
+                    'Nova Transação',
+                  ),
+                  textColor: Colors.purple,
+                  onPressed: () {
+                    print(titleController.text);
+                    print(valueController.text);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
