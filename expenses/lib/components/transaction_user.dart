dfbb87227e322f0a0c44b650a2a84c7127223904
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-02-24 10:53:34 -0300

    Criação do método _addTransaction

--- expenses/lib/components/transaction_user.dart
+++ expenses/lib/components/transaction_user.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:expenses/models/transaction.dart';
 import 'transaction_form.dart';
@@ -29,6 +31,20 @@ class _TransactionUserState extends State<TransactionUser> {
       date: DateTime.now(),
     ),
   ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
