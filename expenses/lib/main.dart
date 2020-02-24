c268600ceac0ee9649e4a1e79b540cb984d8e4ec
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-02-24 12:08:13 -0300

    Fechamento do modal

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -54,6 +54,8 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _transactions.add(newTransaction);
     });
+
+    Navigator.of(context).pop();
   }
 
   _openTransactionFormModal(BuildContext context) {
